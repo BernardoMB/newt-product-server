ba0227704e08d6aac98d5c778cfdb2bf6f4cd93d
Pibomeister <edy7picazo@gmail.com>
2018-12-17 08:25:41 -0600

    Updated purchase schema

--- src/data-access/schemas/PurchaseSchema.ts
+++ src/data-access/schemas/PurchaseSchema.ts
@@ -10,19 +10,22 @@ const status = {
   _id: false
 };
 
-const schema: Schema = new Schema({
-  product: { type: String, required: true },
-  user: { type: Schema.Types.ObjectId, required: true },
-  destination: { type: String, required: true },
-  status: { type: Number, required: true },
-  createdAt: { type: Date, required: true },
-  updatedAt: { type: Date, required: true },
-  amount: { type: Number, required: true },
-  confirmationNumber: { type: String, required: false },
-  comment: { type: String, required: false },
-  extra: { type: Schema.Types.Mixed, required: false },
-  statusLog: { type: [status], required: true }
-});
+const schema: Schema = new Schema(
+  {
+    product: { type: String, required: true },
+    user: { type: Schema.Types.ObjectId, ref: 'user' },
+    destination: { type: String, required: true },
+    status: { type: Number, required: true },
+    amount: { type: Number, required: true },
+    confirmationNumber: { type: String, required: false },
+    comment: { type: String, required: false },
+    extra: { type: Schema.Types.Mixed, required: false },
+    statusLog: { type: [status], required: true }
+  },
+  {
+    timestamps: true
+  }
+);
 
 export const PurchaseSchema: Model<IPurchase> = model<IPurchase>(
   'purchase',

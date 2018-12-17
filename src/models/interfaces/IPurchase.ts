e3705ed0c0ec7edf0b19bf5f04e50965573bf9d1
Pibomeister <edy7picazo@gmail.com>
2018-12-17 13:08:28 -0600

    More changes to the purchase repository

--- src/models/interfaces/IPurchase.ts
+++ src/models/interfaces/IPurchase.ts
@@ -1,6 +1,13 @@
 import { Document } from 'mongoose';
 import { PurchaseStatus } from '../enums/PurchaseStatus';
 
+export interface IPurchaseStatus {
+  updatedAt: Date;
+  code: number;
+  message: string;
+  extra?: any;
+}
+
 export interface IPurchase extends Document {
   product: string;
   user: string;
@@ -12,7 +19,7 @@ export interface IPurchase extends Document {
   confirmationNumber?: number;
   comment?: string;
   extra?: any;
-  statusLog: [{ updatedAt: Date; code: number; message: string; extra?: any }];
+  statusLog: IPurchaseStatus[];
 }
 
 export interface INewPurchase {

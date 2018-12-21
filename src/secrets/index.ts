9afa97b7d679deba23399d218a91bbf831640f3d
faridhajnal <faropumas14_14@hotmail.com>
2018-12-21 11:19:52 -0600

    added production db to test enuironment

--- src/secrets/index.ts
+++ src/secrets/index.ts
@@ -1,4 +1,5 @@
 import { ICredentials } from '../models/interfaces/ICredentials';
 
 const env = process.env.NODE_ENV || 'development';
+console.log('Environment', env);
 export const { credentials }: { credentials: ICredentials } = require(`./${env}`);

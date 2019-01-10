9e344d92e7fd0a748823d5e11a6651e238b7a024
Pibomeister <edy7picazo@gmail.com>
2019-01-10 12:09:33 -0600

    Adds regex maxing for jwt tokens for the Authentication header validator

--- src/routes/middlewares/validators/HeaderValidator.ts
+++ src/routes/middlewares/validators/HeaderValidator.ts
@@ -1,8 +1,5 @@
 import { header } from 'express-validator/check';
 
-export const AuthenticationHeaderValidator = header(
-  'Authorization',
-  '[Authorization] header is not present or invalid'
-).exists();
-// .exists()
-// .matches(/^[A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.?[A-Za-z0-9-_.+/=]*$/);
+export const AuthenticationHeaderValidator = header('Authorization', '[Authorization] header is not present or invalid')
+  .exists()
+  .matches(/^[A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.?[A-Za-z0-9-_.+/=]*$/);
